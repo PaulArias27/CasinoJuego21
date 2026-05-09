8cc4f7e75d85543e2f9eeba0c8d89ebb0c6e432e
PaulArias27 <paularias2727@gmail.com>
2026-05-09 01:29:26 -0500

    PA: se agrego el metodo imprimirNaipe y el costructor de dealer

--- src/com/krakedev/juegos/servicios/Dealer.java
+++ src/com/krakedev/juegos/servicios/Dealer.java
@@ -1,5 +1,47 @@
 package com.krakedev.juegos.servicios;
 
+import java.util.ArrayList;
+
+import com.krakedev.juegos.entidades.Carta;
+
 public class Dealer {
+	
+	private ArrayList<Carta> naipe;
+	
+	
+	//getters and setters
+	public ArrayList<Carta> getNaipe() {
+		return naipe;
+	}
+
+	public void setNaipe(ArrayList<Carta> naipe) {
+		this.naipe = naipe;
+	}
+	
+	
+	//metodos
+	public void generarNaipe() {
+		
+		String[] palos = {"T","CN","CR","D"};
+		String[] valores = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
+		
+		for(String palo: palos) {
+			for(String valor: valores){
+				
+				Carta carta = new Carta();
+				carta.setPalo(palo);
+				carta.setValor(valor);
+				
+				naipe.add(carta);
+			}
+		}
+	}
+	 public void imprimirNaipe() {
+		 for(Carta c : naipe) {
+			 c.imprimir();
+		 }
+	 }
+	
+	
 
 }
